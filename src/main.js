7570c873ebf7808f15080e5403985ef5f2cf3cd6
Matt Sollars <ventaur@gmail.com>
2016-06-18 16:09:14 -0400

    Added an explicit style for the amount of red a payout line exhibits, based on odds.

--- src/main.js
+++ src/main.js
@@ -6,9 +6,12 @@ const testElement = document.getElementById('results');
 for (var count = 0; count < 100; count++) {
     const results = engine.generate();
     const resultsNode = document.createElement('div');
+
     resultsNode.innerText = results;
     if (results.isPayout) {
+        const symbol = results.slotSymbols[0];
         resultsNode.className = 'is-payout';
+        resultsNode.style.color = `rgb(${100 - symbol.odds * 100}%, 0%, 0%)`;
     }
 
     testElement.appendChild(resultsNode);
